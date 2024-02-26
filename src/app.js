933f0a7dddcde95a62226ccbcaf13dfb63a88d8b
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-02-26 01:33:46 +0000

    card generator- html and css

--- src/app.js
+++ src/app.js
@@ -2,41 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = generateRandomCard() {
-  //write your code here
-  let cardNum = [1,2,3,4,5,6,7,8,9,10,11,12];
-  let number = document.getElementById('number');
-  number.addEventListener('click', function()   {
-    let randomNum = cardNum.Math.trunc((Math.random() * 12) +1);
-     if(randomNum === 1) {
-      number.innerHTML('A');
-      } else if ( randomNum === 12 ) {
-        number.innerHTML('K');
-      } else if ( randomNum === 11 ) {
-        number.innerHTML('Q');
-      } else if (randomNum === 10) {
-        number.innerHTML('J');
-      } else {
-        number === randomNum
-      }
+window.onload = function generateRandomCard() {
+  /// Arrays of numcards
+  let cardNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-      document.getElementById("number").innerHTML = number;
-     });
-    
-    
-let cardClub = [spades, diamond, heart, club];
-number.addEventListener('click', function()   {
-  let randomClub = cardClub.Math.trunc((Math.random() * 4) +1);
-   if(randomClub === 1) {
-    club.innerHTML('♥');
-    } else if ( randomClub === 2 ) {
-      club.innerHTML('♣');
-    } else if ( randomClub === 3 ) {
-      club.innerHTML('♠');
-    } else (randomClub === 4) {
-      club.innerHTML === '♦';
-    } 
-    document.getElementById("cardClub").innerHTML = club;
-   });
+  /// function o random number
+  addEventListener("refresh", function() {
+    let randomNum = cardNum.Math.floor(Math.random() * 12 + 1);
+    let number = document.getElementById("cardNumber");
+    if (randomNum === 1) {
+      number.innerHTML("A");
+    } else if (randomNum === 12) {
+      number = "K";
+    } else if (randomNum === 11) {
+      number = "Q";
+    } else if (randomNum === 10) {
+      number = "J";
+    } else {
+      number === randomNum;
+    }
+    number.innerHTML = "number";
+  });
+
+  /// array of clubcard
+  let arrClub = ["spades", "diamond", "heart", "club"];
+
+  /// function random clubcards
+  addEventListener("refresh", function() {
+    let randomClub = arrClub.Math.floor(Math.random() * 4 + 1);
+    if (randomClub === 1) {
+      club = "♥";
+    } else if (randomClub === 2) {
+      club = "♣";
+    } else if (randomClub === 3) {
+      club = "♠";
+    } else randomClub === 4;
+    club = "♦";
+  });
+  let club = document.getElementById("cardClub");
+  club.innerHTML = "";
 };
-  
