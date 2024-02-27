4de6ea351b857875d66ea77266fde663a9b4cd39
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-02-27 17:56:34 +0000

    Final commit - color to Number assigned with club

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ import "./style.css";
 
 // Wait for the window to load before executing the code
 window.onload = () => {
-  setTimeout(() => {
-    const cardElement = document.querySelector(".card");
-    cardElement.classList.remove("spade", "diamond", "heart", "club");
-    cardElement.classList.add(generateRandomClub());
-    cardElement.innerHTML = generateRandomCard();
-  }, 1000 * 1);
+  const constantGenerate = () => {
+    setTimeout(() => {
+      const cardElement = document.querySelector(".card");
+      cardElement.classList.remove("spade", "diamond", "heart", "club");
+      cardElement.classList.add(generateRandomClub());
+      cardElement.innerHTML = generateRandomCard();
+    });
+  };
+  setInterval(constantGenerate, 10000);
+
   // Select the card element and add a random club class
   const cardElement = document.querySelector(".card");
   cardElement.classList.add(generateRandomClub());
@@ -20,11 +24,37 @@ window.onload = () => {
   // Get the button element and add an event listener to generate a new card on click
   const generateBtn = document.querySelector(".cardBtn");
   generateBtn.addEventListener("click", newGenerateCard);
+
+  const sizeHeightInput = document.querySelector(".card-height-input");
+  const sizeWidthInput = document.querySelector(".card-width-input");
+
+  const DEFAULT_CARD_HEIGHT = 300;
+  const DEFAULT_WIDTH_HEIGHT = 150;
+
+  sizeHeightInput.addEventListener("change", function(event) {
+    const currentNumber = event.target.value;
+
+    if (currentNumber && currentNumber != "") {
+      cardElement.style.height = `${currentNumber}px`;
+    } else {
+      cardElement.style.height = `${DEFAULT_CARD_HEIGHT}px`;
+    }
+  });
+
+  sizeWidthInput.addEventListener("change", function(event) {
+    const currentNumber = event.target.value;
+
+    if (currentNumber && currentNumber != "") {
+      cardElement.style.width = `${currentNumber}px`;
+    } else {
+      cardElement.style.width = `${DEFAULT_WIDTH_HEIGHT}px`;
+    }
+  });
 };
 
 // Function to generate a new card on button click
-let newGenerateCard = () => {
-  // Select the card element and update its club class and inner HTML
+let newGenerateCard = event => {
+  event.preventDefault();
   const cardElement = document.querySelector(".card");
   cardElement.classList.remove("spade", "diamond", "heart", "club");
   cardElement.classList.add(generateRandomClub());
@@ -63,8 +93,21 @@ let generateRandomClub = () => {
   let arrClub = ["spade", "diamond", "heart", "club"];
 
   // Get a random index from the arrClub array
-  let randomClub = Math.floor(Math.random() * arrClub.length);
+  let randomClubIndex = Math.floor(Math.random() * arrClub.length);
 
   // Return the randomly selected club class
-  return arrClub[randomClub];
+
+  if (arrClub[randomClubIndex]) {
+    const cardElement = document.querySelector(".card");
+
+    if (
+      arrClub[randomClubIndex] === "spade" ||
+      arrClub[randomClubIndex] === "club"
+    ) {
+      cardElement.style.color = "black";
+    } else {
+      cardElement.style.color = "red";
+    }
+  }
+  return arrClub[randomClubIndex];
 };
